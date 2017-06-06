be9a9c03c615d01ab2440e0490b18abbec8e49e3
ipc103 <iancandysemail@gmail.com>
2017-06-06 09:30:24 -0400

    remove comments

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# Intro to React
-
-## What was hard about building an app with just jQuery?
-
-+ Separation of Concerns
-  + No structure
-  + No real organization of views
-+ Difficult to test
-+ Dependent on what's in the DOM
-+ Imperatively manipulating the DOM
+# Webpack, Babel, State, Events
--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// function createElement(type, props={}, children={}){
-//   return {
-//     $$typeof: Symbol.for('react.element'),
-//     props: Object.assign({}, props, {children: children}),
-//     type: type
-//   }
-// }
-//
-// const el = {
-//   type: 'div',
-//   props: {
-//     children:
-//   },
-//   $$typeof: Symbol.for('react.element')
-// }
-
 function NavBar(props){
   const allowedStyles = ['default', 'inverse']
   if (allowedStyles.includes(props.style)) {
@@ -30,16 +14,5 @@ function NavBar(props){
   )
 }
 
-// const el = React.createElement('div', {kyle: true}, React.createElement('h1', {}, "Hello World"))
 
 ReactDOM.render(NavBar({title: "Ian Rules!", style: 'inverse'}), document.getElementById('root'))
-
-
-{/* <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Brand</a>
-    </div>
-  </div>
-</nav> */}
