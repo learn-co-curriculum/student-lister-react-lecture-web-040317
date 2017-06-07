cffdf1425bcd96e80537dd20b6b4646254b92bea
ipc103 <iancandysemail@gmail.com>
2017-06-07 10:22:15 -0400

    adds challenge

--- src/components/StudentForm.js
+++ src/components/StudentForm.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+
+// Here, I need to render a form with one input for the students name
+// I need to keep track of what the value is in that input at any given time
+// Whenever the form is submitted, I need some way to tell the StudentPage to add that name to the state
 export default function StudentForm(props){
   return(
     <p>Students Form</p>
